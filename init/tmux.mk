3a2e33562101bf840dc353dfb257742772d5bf02
Martin Holman <me@martinholman.co.nz>
2022-08-13 16:24:48 -0700

    Bug fix tmux

--- init/tmux.mk
+++ init/tmux.mk
@@ -1,11 +1,15 @@
 TMUX=$(HOME)/.tmux
 
+.PHONEY: all
+
+all: $(HOME)/.tmux.conf $(HOME)/.tmux.conf.local
+	@echo "TMUX Done."
+
 $(HOME)/.tmux.conf: $(TMUX)
 	@ln -s -f $(TMUX)/.tmux.conf $@
 
 $(HOME)/.tmux.conf.local: $(TMUX)
 	@cp $(TMUX)/.tmux.conf.local $@
-
 
 $(TMUX):
 	@mkdir -p $(TMUX)
