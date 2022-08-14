f34cb408f9740fcd35f581a9b1aae40f41606504
Martin Holman <me@martinholman.co.nz>
2022-08-13 19:28:37 -0700

    Version control tmux config

--- init/tmux.mk
+++ init/tmux.mk
@@ -2,14 +2,11 @@ TMUX=$(HOME)/.tmux
 
 .PHONEY: all
 
-all: $(HOME)/.tmux.conf $(HOME)/.tmux.conf.local $(HOME)/.p10k.zsh
+all: $(HOME)/.tmux.conf
 	@echo "TMUX Done."
 
 $(HOME)/.tmux.conf: $(TMUX)
 	@ln -s $(TMUX)/.tmux.conf $@
-
-$(HOME)/.tmux.conf.local: $(TMUX)
-	@cp $(TMUX)/.tmux.conf.local $@
 
 $(TMUX):
 	@mkdir -p $(TMUX)
